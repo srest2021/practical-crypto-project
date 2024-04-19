f1e7eaa2faa4f63a06101d90134359032f86091a
Sofia Rest <srest2021@gmail.com>
2024-04-19 13:49:54 -0400

    parse identities and recipients

--- hybrid.go
+++ hybrid.go
@@ -7,3 +7,21 @@ type HybridRecipient struct {
 type HybridIdentity struct {
 	secretXKey, secretKKey, ourXPublicKey, ourKPublicKey []byte
 }
+
+func createHybridIdentity(x25519_i *X25519Identity, kyber_i *KyberIdentity) (*HybridIdentity, error) {
+	i := &HybridIdentity{
+		secretXKey:    x25519_i.secretKey,
+		secretKKey:    kyber_i.secretKey,
+		ourXPublicKey: x25519_i.ourPublicKey,
+		ourKPublicKey: kyber_i.ourPublicKey,
+	}
+	return i, nil
+}
+
+func createHybridRecipient(x25519_r *X25519Recipient, kyber_r *KyberRecipient) (*HybridRecipient, error) {
+	r := &HybridRecipient{
+		theirXPublicKey: x25519_r.theirPublicKey,
+		theirKPublicKey: kyber_r.theirPublicKey,
+	}
+	return r, nil
+}
